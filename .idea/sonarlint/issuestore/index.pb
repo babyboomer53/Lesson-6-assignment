e9956e133fb7098b366fae11c1b4a40c323f0357
Edgar Cole <edgarcolejr@gmail.com>
2021-03-02 21:00:17 -0800

    Looks like I've got the server side working as well!

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,7 @@ c
 3src/EDU/UCSD/Extension/Lesson6NetworkingServer.java,4\0\40ec3c1c523e6f1006c1f7928ea673c651f29720
 c
 3src/EDU/UCSD/Extension/Lesson6NetworkingClient.java,e\8\e85289fddec1c93373980567e0da3c3e29d19956
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+G
+Lesson-6-Assignment.iml,c\c\cc7c5381ddb4ff31d2f1feeee60d18de9b912010
--- src/EDU/UCSD/Extension/Lesson6NetworkingServer.java
+++ src/EDU/UCSD/Extension/Lesson6NetworkingServer.java
@@ -1,4 +1,148 @@
 package EDU.UCSD.Extension;
 
-public class Lesson6NetworkingServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+/**
+ * This class handles the client input for one server socket connection.
+ */
+class ThreadedEchoHandler implements Runnable {
+    private Socket incoming;
+
+    /**
+     * Constructs a handler.
+     *
+     * @param incomingSocket the incoming socket
+     */
+    public ThreadedEchoHandler(Socket incomingSocket) {
+        incoming = incomingSocket;
+    }
+
+    public void run() {
+        try (InputStream inStream = incoming.getInputStream();
+             OutputStream outStream = incoming.getOutputStream();
+             var in = new Scanner(inStream, StandardCharsets.UTF_8);
+             var out = new PrintWriter(
+                     new OutputStreamWriter(outStream, StandardCharsets.UTF_8),
+                     true /* autoFlush */)) {
+
+            out.printf("%n%s%n%s%n%n%n%s%n%s%n%s%n%s%n%s%n%s%n",
+                    "HTTP/1.0 200 OK",
+                    "Content-Type: text/html",
+                    "<html>",
+                    "<head><title>Java Networking</title></head>",
+                    "<body>",
+                    "<hl>Java Networking</hl>",
+                    "</body>",
+                    "</html>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
+public class Lesson6NetworkingServer {
+
+    private int port;
+
+    public Lesson6NetworkingServer(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public static void syntaxSummary() {
+        var commandName = "Lesson6NetworkingClient";
+        System.out.printf("%n%-7s%-24s%-18s%s%n%-7s%-24s%-18s%s%n",
+                "Usage:",
+                commandName,
+                "[--help]",
+                "# Displays this command syntax summary",
+                "",
+                commandName,
+                "{--port <number>}",
+                "# Listen for connections on port <number>");
+    }
+
+    public void startServer() {
+
+        try (var serverSocket = new ServerSocket(port)) {
+            int index = 1;
+
+            while (true) {
+                Socket incoming = serverSocket.accept();
+                System.out.println("Spawning " + index);
+                Runnable runnable = new ThreadedEchoHandler(incoming);
+                var thread = new Thread(runnable);
+                thread.start();
+                index++;
+            }
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%nObject of: %s%nPort: %s%n",
+                this.getClass().getSimpleName(),
+                this.getPort());
+    }
+
+    public static void main(String[] options) throws IOException {
+        String option = null;
+        int port = 0;
+
+        try {
+            option = options[0];
+        } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+            System.err.println("\nOops, a required option is missing!");
+            EDU.UCSD.Extension.Lesson6NetworkingServer.syntaxSummary();
+            System.exit(1);
+        }
+        // Process the commandline options…
+        for (int index = 0; index < options.length; index++) {
+            switch (options[index]) {
+                case "--help":
+                    EDU.UCSD.Extension.Lesson6NetworkingServer.syntaxSummary();
+                    System.exit(0);
+                    // break;
+                case "--port":
+                    try {
+                        port = Integer.parseInt(options[++index]);
+                    } catch (NumberFormatException | ArrayIndexOutOfBoundsException exception) {
+                        // I know! Modifying the control variable from within the loop is a no-no!
+                        System.err.printf("%nThe \"%s\" option requires a numeric argument.%n", options[--index]);
+                        EDU.UCSD.Extension.Lesson6NetworkingServer.syntaxSummary();
+                        System.exit(1);
+                    }
+                    break;
+                default:
+                    System.err.printf("%n\"%s\" is not a valid option!%n", options[index]);
+                    EDU.UCSD.Extension.Lesson6NetworkingServer.syntaxSummary();
+                    System.exit(1);
+            }
+        }
+
+        if (port == 0) {
+            System.err.println("\nThe \"--port\" option is required!");
+            EDU.UCSD.Extension.Lesson6NetworkingClient.syntaxSummary();
+        } else {
+            EDU.UCSD.Extension.Lesson6NetworkingServer lesson6NetworkingServer = new EDU.UCSD.Extension.Lesson6NetworkingServer(port);
+            System.out.println(lesson6NetworkingServer);
+            lesson6NetworkingServer.startServer();
+        }
+    }
+
+}
+
